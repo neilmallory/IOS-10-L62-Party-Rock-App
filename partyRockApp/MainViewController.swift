71abed8ac4ba332c6f96ae2fa9f2e819b82407ec
neilmallory <neilmallory@neils-macbook-pro.local>
2017-05-18 15:16:52 +0100

    Lecture 62 complete Basic UI completed

---
+++ partyRockApp/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  partyRockApp
+//
+//  Created by neil mallory on 18/05/2017.
+//  Copyright © 2017 neil mallory. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+}
+
